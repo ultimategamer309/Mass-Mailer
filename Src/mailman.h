d2f35b046945fc8d403fd9639b75e0eb55d8b2e5
ultimategamer309 <50726496+ultimategamer309@users.noreply.github.com>
2021-01-12 22:03:53 -0700

    Improve look of Code and Performance Speed
    Created class of inline static variables to hold variables that where commonly used but rarely changed. Improved performance speed and limited memory usage with better use of references and pointers.

--- Src/mailman.h
+++ Src/mailman.h
@@ -13,29 +13,13 @@
 class mailman
 {
 public:
-	mailman(std::vector<std::string> recipiants,
-		std::string from,
-		std::string senderName,
-		std::string subject,
-		std::vector<std::string> message,
-		std::string key,
-		int threads,
-		bool isHtml = false,
-		std::string server = "0");
+	mailman(std::vector<std::string>& recipiants, int threads);
 
 	mailman();
 
 	mailman(const mailman&);
 
-	void set(std::vector<std::string> recipiants,
-		std::string from,
-		std::string senderName,
-		std::string subject,
-		std::vector<std::string> message,
-		std::string key,
-		int threads,
-		bool isHtml = false,
-		std::string server = "0");
+	void set(std::vector<std::string>& recipiants, int threads);
 
 	~mailman();
 
@@ -46,28 +30,16 @@ public:
 	double getProgressAsPercent();
 	int getCompleted();
 	int getRemaining();
-	std::vector<std::string> getRecipiants();
+	std::vector<std::string>* getRecipiants();
 
 private:
 	static bool isValid(std::string to,
-		std::string from,
-		std::string senderName,
-		std::string subject,
-		std::vector<std::string> message,
-		std::string secret,
-		std::vector<std::string> *validStrings,
-		std::string server = "");
+		std::vector<std::string>* validStrings);
 
-	static void send(std::string to,
-		std::string from,
-		std::string senderName,
-		std::string subject,
-		std::vector<std::string> message,
-		std::string secret,
-		bool html = false,
-		std::string server = "");
+	static void send(std::string to);
 
-	std::vector<std::string> recipiants;
+	std::vector<std::string> *recipiants;
+	std::vector<std::string> recipiants1;
 	std::string from;
 	std::string senderName;
 
